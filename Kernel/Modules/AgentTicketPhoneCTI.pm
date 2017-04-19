334cf7be6163aa39d50f01cf2c0eeacd8a5d2fef
Denny Bresch <db@znuny.com>
2017-04-19 15:40:58 +0200

    Tieded code.

--- Kernel/Modules/AgentTicketPhoneCTI.pm
+++ Kernel/Modules/AgentTicketPhoneCTI.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2012-2016 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
