3bc1c01bc8933d4b1f0aa628f897d63bccdbdd5c
Thorsten Eckel <te@znuny.com>
2017-04-20 09:31:03 +0200

    Applied QA by @jepf.

--- Kernel/Language/de_Znuny4OTRSCTI.pm
+++ Kernel/Language/de_Znuny4OTRSCTI.pm
@@ -20,7 +20,7 @@ sub Data {
     $Self->{Translation}->{"Frontend module registration for the agent interface. E. g. 'otrs/index.pl?Action=AgentTicketPhoneCTI;CallerID=XXX;MSN=XXX'."} = "Frontend-Modul-Registrierung für die Agent-Schnittstelle. Z.B. 'otrs/index.pl?Action=AgentTicketPhoneCTI;CallerID=XXX;MSN=XXX'.";
     $Self->{Translation}->{"Expert Setting - not needed in standard setup! Defines the Action based on MSN, e. g. route incoming calls to different frontend modules (Key=Number,Value=Frontend-Module e. g. Key=1222,Value=AgentTicketPhoneHotline)."} = "Expert Setting - wird bei der Standardinstallation nicht benötigt! Definiert die Aktion basierend auf MSN, e. G. Routet eingehende Anrufe an verschiedene Frontend-Module (Key = Number, Value = Frontend-Modul e. G. Key = 1222, Value = AgentTicketPhoneHotline).";
     $Self->{Translation}->{"This configuration defines the maximum of CustomerUsers that should get added to the the AgentTicketPhone view."} = "Diese Konfiguration definiert das Maximum der CustomerUsers, die in der AgentTicketPhone-Ansicht hinzugefügt werden sollen.";
-    $Self->{Translation}->{"This configuration defines the regular expression for the CallerID."} = "Diese Konfiguration definiert den regulären Ausdruck für die CallerID.";
+    $Self->{Translation}->{"Regular expression for the CallerID."} = "Regulärer Ausdruck für die CallerID.";
 
     return 1;
 }
