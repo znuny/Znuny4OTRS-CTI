58c77b7eee928dedbadfa3aa06b4b1dc2fff66e0
Denny Bresch <db@znuny.com>
2019-06-04 12:14:58 +0200

    Fixed issue4 - CustomerUser config should not get overwritten #4

--- Kernel/Config/Files/Znuny4OTRSCTI.pm
+++ Kernel/Config/Files/Znuny4OTRSCTI.pm
@@ -1,6 +1,6 @@
 # VERSION:1.1
 # --
-# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2019 Znuny GmbH, http://znuny.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -12,103 +12,33 @@ package Kernel::Config::Files::Znuny4OTRSCTI;
 use strict;
 use warnings;
 
+use Kernel::System::VariableCheck qw(:all);
+
 use utf8;
 
 sub Load {
     my ( $File, $Self ) = @_;
 
     # changed config to also use phone number as seach field
-    $Self->{CustomerUser} = {
-        Name   => 'Database Backend',
-        Module => 'Kernel::System::CustomerUser::DB',
-        Params => {
+    SOURCE:
+    for my $Count ( '', 1 .. 10 ) {
 
-            # if you want to use an external database, add the
-            # required settings
-            #            DSN => 'DBI:odbc:yourdsn',
-            #            DSN => 'DBI:mysql:database=customerdb;host=customerdbhost',
-            #            User => '',
-            #            Password => '',
-            Table => 'customer_user',
+        next SOURCE if !$Self->{"CustomerUser$Count"};
 
-            # if your frontend is unicode and the charset of your
-            # customer database server is iso-8859-1, use these options.
-            #           SourceCharset => 'iso-8859-1',
-            #           DestCharset => 'utf-8',
+        my $Map = $Self->{"CustomerUser$Count"}->{Map};
+        next SOURCE if !IsArrayRefWithData($Map);
 
-            # CaseSensitive will control if the SQL statements need LOWER()
-            #   function calls to work case insensitively. Setting this to
-            #   1 will improve performance dramatically on large databases.
-            CaseSensitive => 0,
-        },
+        ENTRY:
+        for my $MapEntry ( @{$Map} ) {
+            next ENTRY if $MapEntry->[0] ne 'UserPhone';
 
-        # customer unique id
-        CustomerKey => 'login',
+            my $PhoneKey = $MapEntry->[2];
 
-        # customer #
-        CustomerID             => 'customer_id',
-        CustomerValid          => 'valid_id',
-        CustomerUserListFields => [ 'first_name', 'last_name', 'email' ],
+            last ENTRY if grep { $PhoneKey eq $_ } @{ $Self->{"CustomerUser$Count"}->{CustomerUserSearchFields} };
 
-        #        CustomerUserListFields => ['login', 'first_name', 'last_name', 'customer_id', 'email'],
-        #        CustomerUserSearchFields           => [ 'login', 'first_name', 'last_name', 'customer_id' ],
-        CustomerUserSearchFields           => [ 'login', 'first_name', 'last_name', 'customer_id', 'phone' ],
-        CustomerUserSearchPrefix           => '*',
-        CustomerUserSearchSuffix           => '*',
-        CustomerUserSearchListLimit        => 250,
-        CustomerUserPostMasterSearchFields => ['email'],
-        CustomerUserNameFields     => [ 'title', 'first_name', 'last_name' ],
-        CustomerUserEmailUniqCheck => 1,
-
-        #        # show now own tickets in customer panel, CompanyTickets
-        #        CustomerUserExcludePrimaryCustomerID => 0,
-        #        # generate auto logins
-        #        AutoLoginCreation => 0,
-        #        # generate auto login prefix
-        #        AutoLoginCreationPrefix => 'auto',
-        #        # admin can change customer preferences
-        #        AdminSetPreferences => 1,
-        #        # use customer company support (reference to company, See CustomerCompany settings)
-        #        CustomerCompanySupport => 1,
-        #        # cache time to live in sec. - cache any database queries
-        #        CacheTTL => 0,
-        #        # just a read only source
-        #        ReadOnly => 1,
-        Map => [
-
-        # note: Login, Email and CustomerID needed!
-        # var, frontend, storage, shown (1=always,2=lite), required, storage-type, http-link, readonly, http-link-target
-            [ 'UserTitle',     'Title',     'title',      1, 0, 'var', '', 0 ],
-            [ 'UserFirstname', 'Firstname', 'first_name', 1, 1, 'var', '', 0 ],
-            [ 'UserLastname',  'Lastname',  'last_name',  1, 1, 'var', '', 0 ],
-            [ 'UserLogin',     'Username',  'login',      1, 1, 'var', '', 0 ],
-            [ 'UserPassword',  'Password',  'pw',         0, 0, 'var', '', 0 ],
-            [ 'UserEmail',     'Email',     'email',      1, 1, 'var', '', 0 ],
-
-#            [ 'UserEmail',      'Email', 'email',           1, 1, 'var', '$Env{"CGIHandle"}?Action=AgentTicketCompose&ResponseID=1&TicketID=$Data{"TicketID"}&ArticleID=$Data{"ArticleID"}', 0 ],
-            [ 'UserCustomerID', 'CustomerID', 'customer_id', 0, 1, 'var', '', 0 ],
-
-            #            [ 'UserCustomerIDs', 'CustomerIDs', 'customer_ids', 1, 0, 'var', '', 0 ],
-            [ 'UserPhone',   'Phone',   'phone',    1, 0, 'var', '', 0 ],
-            [ 'UserFax',     'Fax',     'fax',      1, 0, 'var', '', 0 ],
-            [ 'UserMobile',  'Mobile',  'mobile',   1, 0, 'var', '', 0 ],
-            [ 'UserStreet',  'Street',  'street',   1, 0, 'var', '', 0 ],
-            [ 'UserZip',     'Zip',     'zip',      1, 0, 'var', '', 0 ],
-            [ 'UserCity',    'City',    'city',     1, 0, 'var', '', 0 ],
-            [ 'UserCountry', 'Country', 'country',  1, 0, 'var', '', 0 ],
-            [ 'UserComment', 'Comment', 'comments', 1, 0, 'var', '', 0 ],
-            [ 'ValidID',     'Valid',   'valid_id', 0, 1, 'int', '', 0 ],
-        ],
-
-        # default selections
-        Selections => {
-
-            #            UserTitle => {
-            #                'Mr.' => 'Mr.',
-            #                'Mrs.' => 'Mrs.',
-            #            },
-        },
-    };
+            push @{ $Self->{"CustomerUser$Count"}->{CustomerUserSearchFields} }, $PhoneKey;
+        }
+    }
 
     return 1;
 }
