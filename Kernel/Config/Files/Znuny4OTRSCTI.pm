9efc8264027ab3f9ee080e5ea7c61547956dfd88
Denny Korsukéwitz <db@znuny.com>
2020-01-20 17:10:17 +0100

    CTI MaxUsers with several possible addresses

--- Kernel/Config/Files/Znuny4OTRSCTI.pm
+++ Kernel/Config/Files/Znuny4OTRSCTI.pm
@@ -1,6 +1,6 @@
 # VERSION:1.1
 # --
-# Copyright (C) 2012-2019 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
