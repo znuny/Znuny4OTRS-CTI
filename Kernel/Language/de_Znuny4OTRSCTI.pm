62ed4a7c5b87ee33e3a42370193e24f5b2eb1239
Denny Korsukéwitz <dk@znuny.com>
2021-01-20 11:28:13 +0100

    Tidied code.

--- Kernel/Language/de_Znuny4OTRSCTI.pm
+++ Kernel/Language/de_Znuny4OTRSCTI.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2021 Znuny GmbH, http://znuny.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
