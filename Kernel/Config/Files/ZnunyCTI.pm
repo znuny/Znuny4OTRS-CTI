73f6bc01ef0e0561897f1a0127e70e1ddd47cdae
Niklas Schmitt <ns@znuny.com>
2023-01-06 11:28:43 +0100

    Applied ZnunyCodePolicy.

--- Kernel/Config/Files/ZnunyCTI.pm
+++ Kernel/Config/Files/ZnunyCTI.pm
@@ -1,6 +1,6 @@
 # VERSION:1.1
 # --
-# Copyright (C) 2012-2022 Znuny GmbH, https://znuny.com/
+# Copyright (C) 2012 Znuny GmbH, https://znuny.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
